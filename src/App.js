0ab29a85719f52fe9693ddff3f17e548c3728cd6
Programmer-Mehadi <mehadi.developer@gmail.com>
2023-01-22 01:19:43 +0600

    add employee done.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
+import { Toaster } from 'react-hot-toast';
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import router from './Routes/Routes';
-import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     // <div className="bg-gradient-to-r from-indigo-400 to-cyan-400">
-    <div className='max-w-[1600px] mx-auto'>
+    <div className='max-w-[1700px] mx-auto'>
       <RouterProvider router={router}>
-
       </RouterProvider>
       <Toaster />
     </div>
